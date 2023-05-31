ada7889b8f9e1bff36e2aa7280ac7791dc36c2ff
Mayank <7mayank.agarwal@gmail.com>
2023-05-31 21:27:37 +0530

    setup apollo server

--- index.ts
+++ index.ts
@@ -1,2 +1,20 @@
-const age: number=24001
-console.log(age)
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from '@apollo/server/standalone';
+import typeDefs from "./src/schema";
+import { Query } from "./src/resolvers"
+
+const resolvers = {
+  Query
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+async function startServer()
+{
+  const { url } = await startStandaloneServer(server);
+  console.log(`🚀 Server ready at ${url}`);
+}
+startServer()
